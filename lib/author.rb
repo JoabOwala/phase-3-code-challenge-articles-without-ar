2566c2eeb88ed1f8175e340c160115e4b83d9549
Joab Owala <owalajoab@gmail.com>
2023-05-28 22:07:21 +0300

    articles and magazines

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,20 @@
-require 'pry'
+
 
 class Author
   attr_reader :name
 
   def initialize(name)
    @name = name
+   @articles = []
   end
+
+  def articles
+    @articles
+  end
+
+  def magazines
+    @articles.map { |article| article.magazine }.uniq
+  end
+  
 end
 
-binding.pry
